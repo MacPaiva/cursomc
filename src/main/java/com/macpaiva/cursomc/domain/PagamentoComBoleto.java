c688759faa2488731aba92e455ba80e6d84b58b5
Lucas Paiva <lucasm.paiva@hotmail.com>
2022-01-05 18:27:13 -0300

    Atualizacao: utilizando somente JsonIgnore

--- src/main/java/com/macpaiva/cursomc/domain/PagamentoComBoleto.java
+++ src/main/java/com/macpaiva/cursomc/domain/PagamentoComBoleto.java
@@ -16,12 +16,9 @@ public class PagamentoComBoleto extends Pagamento {
 	
 	@JsonFormat(pattern="dd/MM/yyyy")
 	private Date dataPagamento;
-	
-	
-	public PagamentoComBoleto() {
 		
+	public PagamentoComBoleto() {
 	}
-
 
 	public PagamentoComBoleto(Integer id, EstadoPagamento estado, Pedido pedido, Date dataVencimento, Date dataPagamento) {
 		super(id, estado, pedido);
@@ -29,21 +26,17 @@ public class PagamentoComBoleto extends Pagamento {
 		this.dataVencimento = dataVencimento;
 	}
 
-
 	public Date getDataVencimento() {
 		return dataVencimento;
 	}
-
 
 	public void setDataVencimento(Date dataVencimento) {
 		this.dataVencimento = dataVencimento;
 	}
 
-
 	public Date getDataPagamento() {
 		return dataPagamento;
 	}
-
 
 	public void setDataPagamento(Date dataPagamento) {
 		this.dataPagamento = dataPagamento;
